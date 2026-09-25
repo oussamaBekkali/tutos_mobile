c1a1c74ae26ca8dd03a4b7cd9cf3ae1286b5462a
oussama <bakkali.oussama.solicode@gmail.com>
2026-09-25 10:55:09 +0000

    Encapsuler les données d’un objet

--- Construire une fonctionnalité avec des objets/Créer et manipuler un objet/backend/categorie.php
+++ Construire une fonctionnalité avec des objets/Créer et manipuler un objet/backend/categorie.php
@@ -1,23 +1,61 @@
 <?php
-class categorie
+
+class Categorie
 {
-    public $id;
-    public $nom;
-    public $couleur;
-    public $icon;
+    private int $id;
+    private string $nom;
+    private string $couleur;
+    private string $icon;
 
-    public function __construct($id, $nom, $couleur, $icon)
+    public function getId(): int
     {
-        $this->id = $id;
-        $this->nom = $nom;
-        $this->couleur = $couleur;
-        $this->icon = $icon;
+        return $this->id;
     }
 
-    public function afficher()
+    public function getNom(): string
     {
-        echo "-Nom " . $this->nom;
-        echo " -Couleur " . $this->couleur;
-        echo " -Icone " . $this->icon;
+        return $this->nom;
     }
-};
+
+    public function getCouleur(): string
+    {
+        return $this->couleur;
+    }
+
+    public function getIcon(): string
+    {
+        return $this->icon;
+    }
+
+    public function setId(int $nId): void
+    {
+        $this->id = $nId;
+    }
+
+    public function setNom(string $nNom): void
+    {
+        if (strlen($nNom) > 2) {
+            $this->nom = $nNom;
+        } else {
+            echo "Erreur : Nom trop court !<br>";
+        }
+    }
+
+    public function setCouleur(string $nCouleur): void
+    {
+        if (strlen($nCouleur) > 2) {
+            $this->couleur = $nCouleur;
+        } else {
+            echo "Erreur : Couleur trop courte !<br>";
+        }
+    }
+
+    public function setIcon(string $nIcon): void
+    {
+        if (strlen($nIcon) > 2) {
+            $this->icon = $nIcon;
+        } else {
+            echo "Erreur : Icône trop courte !<br>";
+        }
+    }
+}
--- Construire une fonctionnalité avec des objets/Créer et manipuler un objet/backend/test-categorie.php
+++ Construire une fonctionnalité avec des objets/Créer et manipuler un objet/backend/test-categorie.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'categorie.php';
 
-$cat = new categorie(1, "iphons", "blue", "apple");
-$cat->afficher();
+$cat = new categorie();
+$cat->setNom("iphone");
+$cat->setId(2);
+$cat->setCouleur("black");
+$cat->setIcon("apple");
+
+echo $cat->getCouleur() . "<br>";
+echo $cat->getIcon() . "<br>";
+echo $cat->getId() . "<br>";
+echo $cat->getNom() . "<br>";
